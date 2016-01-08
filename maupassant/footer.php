09831792823a86dd32afe7a523d545ba265db3d2
王雪松 <xswang@outlook.com>
2016-01-08 16:46:48 +0800

    init
    init typecho and maupassant

---
+++ maupassant/footer.php
@@ -0,0 +1,11 @@
+		</div>
+    </div>
+</div>
+<footer id="footer">
+	<div class="container">
+		&copy; <?php echo date('Y'); ?> <a rel="nofollow" href="<?php $this->options->siteUrl(); ?>"><?php $this->options->title(); ?></a>. <a href="http://www.miitbeian.gov.cn/">津ICP备12007193号-2</a>.
+	</div>
+</footer>
+<?php $this->footer(); ?>
+</body>
+</html>
